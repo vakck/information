367d3101ed4540252048854b93c6126e9dfcbd2d
vakck <qing6738239047164@163.com>
2021-06-06 17:36:57 +0800

    mysql数据库的配置信息

--- manage.py
+++ manage.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+class Config(object):
+    """项目配置信息"""
+    DEBUG = True
+
+    # mysql数据库配置信息
+    # 数据库连接配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1@3306/information19"
+    # 关闭数据库修改跟踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# 创建app对象
 app = Flask(__name__)
+# 将配置类注册到app上
+app.config.from_objeect(Config)
+
+# 2.创建数据库对象
+db = SQLAlchemy(app)
 
 
 @app.route('/')
