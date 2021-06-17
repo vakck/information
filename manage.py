b82c32375b9ca7917534f46c65b3020f680bce20
vakck <qing6738239047164@163.com>
2021-06-17 14:58:57 +0800

    开启csrf保护机制

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect, generate_csrf
 
 
 class Config(object):
@@ -22,13 +23,21 @@ class Config(object):
 # 创建app对象
 app = Flask(__name__)
 # 将配置类注册到app上
-app.config.from_objeect(Config)
+app.config.from_object(Config)
 
 # 2.创建数据库对象
 db = SQLAlchemy(app)
 
 # 3.创建redis数据库对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_NUM)
+
+"""
+# 4.开启csrf保护机制
+1.自动获取cookie中的csrf_token,
+2.自动获取ajax请求头中的csrf_token
+3.自己校验这两个值是否一致
+"""
+csrf = CSRFProtect(app)
 
 
 @app.route('/')
